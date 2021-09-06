08dbfd4f3662016a999d784d25c1f4d3eee5a433
Khoa1468 <khoalam1468@gmail.com>
2021-09-06 16:13:27 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nSoNguyen;
+	nSoNguyen = 69;
+	nSoNguyen = (6 + 9) * (6 - 9);
+	cout << nSoNguyen << endl;
+
+	system("pause");
+	return 0;
+}
+
